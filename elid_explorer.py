032bd292e4395c7924136dc53bba82b2c1dc4dd8
altshiftj <j.fitz@tu-braunschweig.de>
2024-11-28 10:03:31 +0100

    need to discuss best data extraction and policy practices for elids

--- elid_explorer.py
+++ elid_explorer.py
@@ -1,6 +1,7 @@
 from rsciio import phenom
 import imagecodecs
 import json
+import re
 
 file_path = r'D:\Repos\ipat_data_watchdog\test_elid\test.elid'
 
@@ -13,10 +14,21 @@ def load_elid_file(file_path: str) -> None:
 
     return elid_file
 
+def extract_elid_metadata(elid_file: dict) -> dict:
+    '''
+    Extracts the metadata from the ELID file
+    '''
+    # Patterns
+    image_pattern = r"^Image \d+$"
+    analysis_pattern = r"^Image \d+, (Spot|Line|Region|Map) \d+$"
+
+    for i in elid_file.len():
+        if not re.match(image_pattern, elid_file[i]['metadata']['General']['title']):
+            print(i)
+
+    return metadata
+
 if __name__ == '__main__':
     elid = load_elid_file(file_path)
 
-    # save the elid dictionary to a json file
-    with open('elid.json', 'w') as f:
-        json.dump(elid, f)
 
